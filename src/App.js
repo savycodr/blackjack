a752dc34370b2033cc35ac64e8ccf693ac645a33
savycodr <46404719+savycodr@users.noreply.github.com>
2019-08-03 22:51:44 -0400

    corrected asynchronous bug

--- src/App.js
+++ src/App.js
@@ -31,46 +31,56 @@ constructor(props){
     playerCards: [],
     dealerCards: [],
     deckId: 0,
-    message: "Welcome"
+    message: "Welcome",
+    disableBtn: false
   }
   this.handleHitClick = this.handleHitClick.bind(this);
   this.handleStandClick = this.handleStandClick.bind(this);
+  this.handleResetClick = this.handleResetClick.bind(this);
+
 }
 
 
   componentDidMount(){
-    // shuffle the deck
-    API.shuffle().then(res=>{
-      this.setState({deckId:res.data.deck_id});
-      console.log("cards are shuffled " + this.state.deckId);
 
-      // deal 4 cards
-      API.deal(this.state.deckId, 4).then(res=>{
-        console.log("cards are dealt");
-  
-        let playerCards = []
-        playerCards.push(
-          {image: res.data.cards[0].image,
-          value: res.data.cards[0].value});
-        playerCards.push(
-          { image: res.data.cards[1].image,
-            value: res.data.cards[1].value});
-        this.setState({playerCards: playerCards});
+    this.init();
+  }
+
+  init(){
+
+        // shuffle the deck
+        API.shuffle().then(res=>{
+          this.setState({deckId:res.data.deck_id});
+          console.log("cards are shuffled " + this.state.deckId);
     
-        let dealerCards = []
-        dealerCards.push(
-          {image: res.data.cards[2].image,
-          value: res.data.cards[2].value});
-          dealerCards.push(
-          { image: res.data.cards[3].image,
-            value: res.data.cards[3].value});
-        this.setState({dealerCards: dealerCards});
-  
-        console.log("playerCards ", playerCards );
-        console.log("dealerCards ", dealerCards );
-
-     })
-    }).catch(error =>console.log(error));
+          // deal 4 cards
+          API.deal(this.state.deckId, 4).then(res=>{
+            console.log("cards are dealt");
+      
+            let playerCards = []
+            playerCards.push(
+              {image: res.data.cards[0].image,
+              value: res.data.cards[0].value});
+            playerCards.push(
+              { image: res.data.cards[1].image,
+                value: res.data.cards[1].value});
+            this.setState({playerCards: playerCards});
+        
+            let dealerCards = []
+            dealerCards.push(
+              {image: res.data.cards[2].image,
+              value: res.data.cards[2].value});
+              dealerCards.push(
+              { image: res.data.cards[3].image,
+                value: res.data.cards[3].value});
+            this.setState({dealerCards: dealerCards});
+      
+            console.log("playerCards ", playerCards );
+            console.log("dealerCards ", dealerCards );
+    
+         })
+        }).catch(error =>console.log(error));
+    
   }
 
   // Handle the hit button
@@ -88,8 +98,9 @@ constructor(props){
 
       let total = this.getTotalValue(cards);
       if (total >21){
-        console.log("You lose");
-        this.setState({message: "You Lose!"})
+        console.log("1. You lose " + total);
+        this.setState({message: "You Lose!"});
+        this.setState({disableBtn: true});
       }
     
      });
@@ -98,20 +109,26 @@ constructor(props){
 
   handleStandClick(e){
     e.preventDefault();
-
-    let dealerTotal = this.getTotalValue(this.state.dealerCards);
-    let playerTotal = this.getTotalValue(this.state.playerCards);
-
-    dealerTotal = this.hitDealer(dealerTotal);
-
-    if ((dealerTotal > 21)||(dealerTotal<playerTotal)){
-      this.setState({message: "You Win"});
-    } else if (dealerTotal === playerTotal)  {
-      this.setState({message: "You Tie"});
-    } else {
-      this.setState({message: "You Lose"});
-    }
+    // this.hitDealer(this.determineWinner());
+    this.hitDealer();
   }
+
+  determineWinner(){
+  let dealerTotal = this.getTotalValue(this.state.dealerCards);
+  let playerTotal = this.getTotalValue(this.state.playerCards);
+    
+  console.log("dealerTotal ", dealerTotal);
+  console.log("playerTotal ", playerTotal);
+
+  if ((dealerTotal > 21)||(dealerTotal<playerTotal)){
+    this.setState({message: "You Win"});
+  } else if (dealerTotal === playerTotal)  {
+    this.setState({message: "You Tie"});
+  } else if (dealerTotal>playerTotal){
+    this.setState({message: "You Lose"});
+  }
+  this.setState({disableBtn: true});
+}
 
   getTotalValue(cards){
     let total = 0;
@@ -121,7 +138,9 @@ constructor(props){
     return total;
   }
 
-  hitDealer(total){
+  hitDealer(){
+    let total = this.getTotalValue(this.state.dealerCards);
+    console.log("the hitdealer total is at " + total);
     if (total<17)
     {
       API.deal(this.state.deckId, 1).then(res=>{
@@ -135,12 +154,53 @@ constructor(props){
         this.setState({dealerCards: cards});
   
         total = this.getTotalValue(cards);
-        this.hitDealer(total);
+        if (total>17){
+          this.determineWinner();
+        } else {
+          this.hitDealer();
+        }
       })
     } else {
-      return total;
+      this.determineWinner();
     }
   }
+
+
+    // hitDealer(cb){
+  //   let total = this.getTotalValue(this.state.dealerCards);
+  //   console.log("the hitdealer total is at " + total);
+  //   if (total<17)
+  //   {
+  //     API.deal(this.state.deckId, 1).then(res=>{
+  //       console.log("a new card is dealt to the dealer");
+    
+  //       let cards = this.state.dealerCards;
+  //       cards.push(
+  //         {image: res.data.cards[0].image,
+  //         value: res.data.cards[0].value});
+  
+  //       this.setState({dealerCards: cards});
+  
+  //       total = this.getTotalValue(cards);
+  //       this.hitDealer();
+  //     })
+  //   }
+  //   else {
+  //     console.log("I am calling the callback now");
+  //     if (cb) cb();
+  //   }
+
+  // }
+
+  handleResetClick(e){
+    e.preventDefault();
+    this.setState({disableBtn: false});
+    this.setState({message: "Welcome Back"});
+
+    this.init();
+  }
+
+
 
   render(){
     return(
@@ -153,8 +213,9 @@ constructor(props){
           <GameDeck cards={this.state.playerCards}></GameDeck>
         </div>
         <div>
-          <button onClick={this.handleHitClick}>Hit</button>
-          <button onClick={this.handleStandClick}>Stand</button>
+          <button disabled={this.state.disableBtn} onClick={this.handleHitClick} >Hit</button>
+          <button disabled={this.state.disableBtn} onClick={this.handleStandClick}  >Stand</button>
+          <button disabled={!this.state.disableBtn} onClick={this.handleResetClick} >Play Again</button>
         </div>
     </div>
   )}
