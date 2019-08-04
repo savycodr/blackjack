b58309dab0d9330c383c6f6663fd394e29350adf
savycodr <46404719+savycodr@users.noreply.github.com>
2019-08-03 20:12:50 -0400

    Updates

---
+++ precache-manifest.8777b3bc9562156171f5e83499011fbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb1167991091b634eec3b6deb9a2049e",
+    "url": "/blackjack/index.html"
+  },
+  {
+    "revision": "66849b3cacf5fe736bfe",
+    "url": "/blackjack/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b36a0c58eb6671eb1dd7",
+    "url": "/blackjack/static/js/2.9e47259f.chunk.js"
+  },
+  {
+    "revision": "66849b3cacf5fe736bfe",
+    "url": "/blackjack/static/js/main.6cd55f08.chunk.js"
+  },
+  {
+    "revision": "499199909dcd85e1cabf",
+    "url": "/blackjack/static/js/runtime~main.73e4a0a6.js"
+  }
+]);
